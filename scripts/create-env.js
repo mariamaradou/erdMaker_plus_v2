4c3749962615eead5301c44bb44d16ccbd76659e
Maria Maradou <61742470+mariamaradou@users.noreply.github.com>
2021-11-18 02:17:41 +0200

    changed env variables

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const path = `./.env`
 const vars = `
- REACT_APP_ERD_NOTATION_CHEN=${process.env.REACT_APP_ERD_NOTATION_CHEN}
+ REACT_APP_ERD_NOTATION_CHEN=${process.env.REACT_APP_ERD_NOTATION_CHEN}\n
+ NODE_ENV = ${process.env.NODE_ENV}\n
+ REACT_APP_DO_HOST = ${process.env.REACT_APP_DO_HOST}\n
+ REACT_APP_EER_MODEL ==  ${process.env.REACT_APP_EER_MODEL}
 `
 fs.writeFileSync(path, vars)
